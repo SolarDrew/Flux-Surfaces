fd346997fbe6b65f441546913922286a7a3d544b
Drew Leonard <andy.j.leonard@gmail.com>
2016-01-26 13:56:35 +0000

    Drew testing

--- scripts/ib_run_all_array.sh
+++ scripts/ib_run_all_array.sh
@@ -6,16 +6,14 @@
 #$ -pe openmpi-ib 16
 #$ -P mhd
 #$ -q mhd.q
-#$ -N all_periods
+#$ -N fluxS_test
 #$ -j y
 #$ -t 1 
 
 source $HOME/.bashrc
-module load mpi/intel/openmpi/1.8.3
-module load apps/python2-virtual/2.7.6
-workon vtk6
-export PATH=:/home/smq11sjm/.virtualenvs/vtk6/bin:$PATH
-export LD_LIBRARY_PATH=/home/smq11sjm/.virtualenvs/vtk_hdf/lib/vtk-6.1/:$LD_LIBRARY_PATH
+module purge
+module load apps/python/conda
+source activate mpi-sac
 echo $(which python)
 #################################################################
 ################ Set the Parameters for the array ###############
@@ -29,14 +27,14 @@ drivers=('Slog')
 #### Setup and Configure ####
 i=$((SGE_TASK_ID - 1))
 
-BASE_DIR=$HOME/GitHub/SWAT/Flux-Surfaces
-TMP_DIR=$(mktemp -d --tmpdir=/fastdata/smq11sjm/temp_run/)
+BASE_DIR=$HOME/Flux-Surfaces
+TMP_DIR=$(mktemp -d --tmpdir=/fastdata/sm1ajl/temp_run/)
 
 cp -r $BASE_DIR $TMP_DIR
 cd $TMP_DIR/Flux-Surfaces/
 pwd
 
-echo ${periods[i]} ${amps[i]} "${fortamps[i]}"
+#echo ${periods[i]} ${amps[i]} "${fortamps[i]}"
 #./configure.py set driver --period=${periods[i]} --amp=${amps[i]} --fort_amp="${fortamps[i]}";
 ./configure.py set driver --driver=${drivers[i]};
 ./configure.py print;
@@ -46,13 +44,15 @@ echo ${periods[i]} ${amps[i]} "${fortamps[i]}"
 python ./run.py SAC --mpi
 
 #### Run the CODE! ####
-tube_radii=( 'r60' 'r30' 'r10' )
-for tuber in "${tube_radii[@]}"
-do
-    echo $tuber
-    python ./run.py analysis --mpi --tube-r=$tuber
-done
+#tube_radii=( 'r60' 'r30' 'r10' )
+#for tuber in "${tube_radii[@]}"
+#do
+#    echo $tuber
+#    python ./run.py analysis --mpi --tube-r=$tuber
+#done
+#
 ###### I Done it now ########
 rm -r $TMP_DIR
+
 pushover -m "Job "${drivers[i]}" Complete"
 echo "Job "${drivers[i]}" Complete"
